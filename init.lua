edb2f69f560b8ede4756ca4f5a657cbef3ad7aec
auroraops <5239604@qq.com>
2023-03-16 00:31:18 +0800

    autotag

--- init.lua
+++ init.lua
@@ -1,32 +1,32 @@
-require "core.options"
-require "core.keymaps"
-require "plugins.plugins-setup"
-require "plugins.colorscheme"
-require "plugins.nvimtree"
-require "plugins.cmp"
-require "plugins.bufferline"
-require "plugins.lualine"
-require "plugins.lsp"
-require "plugins.telescope"
-require "plugins.treesitter"
-require "plugins.comment"
-require "plugins.gitsigns"
-require "plugins.project"
-require "plugins.toggleterm"
-require "plugins.autopairs"
-require "plugins.alpha"
-require "plugins.whichkey"
-require "plugins.impatient"
-require "plugins.indentline"
-require "plugins.autocmd"
-require "plugins.treesitter-textobj"
-require "plugins.autopairs"
-require "plugins.leap"
-require "plugins.ufo"
-require 'plugins.lazygit'
-require 'plugins.substitute'
-require 'plugins.surround'
-require "user.entire"
-require "plugins.tmux-navigator"
-require "plugins.snip"
-require "plugins.noice"
+require("core.options")
+require("core.keymaps")
+require("plugins.plugins-setup")
+require("plugins.colorscheme")
+require("plugins.nvimtree")
+require("plugins.cmp")
+require("plugins.bufferline")
+require("plugins.lualine")
+require("plugins.lsp")
+require("plugins.telescope")
+require("plugins.treesitter")
+require("plugins.comment")
+require("plugins.gitsigns")
+require("plugins.project")
+require("plugins.toggleterm")
+require("plugins.autopairs")
+require("plugins.alpha")
+require("plugins.whichkey")
+require("plugins.impatient")
+require("plugins.indentline")
+require("plugins.autocmd")
+require("plugins.treesitter-textobj")
+require("plugins.autopairs")
+require("plugins.leap")
+require("plugins.ufo")
+require("plugins.lazygit")
+require("plugins.substitute")
+require("plugins.surround")
+require("user.entire")
+require("plugins.tmux-navigator")
+require("plugins.snip")
+require("plugins.noice")
